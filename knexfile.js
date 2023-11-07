24c2607fe2d3311711a9decc73386ab201ad5a2b
Stephen Im <imssbb@gmail.com>
2023-11-07 09:39:14 -0800

    adjust env

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,14 @@
-require("dotenv").config();
+require('dotenv').config();
 // Update with your config settings.
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  client: "mysql2",
+  client: 'mysql2',
   connection: {
-    database: "instock-api",
-    host: process.env.DB_LOCAL_DBNAME,
+    database: 'instock-api',
+    host: process.env.DB_HOST,
     user: process.env.DB_LOCAL_USER,
     password: process.env.DB_LOCAL_PASSWORD,
   },
