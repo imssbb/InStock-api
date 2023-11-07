e2ab2bcf6344b33f84fa63f59aa8c13e074f38a6
Tenzing P Lama <tenzingpalden520@gmail.com>
2023-11-07 15:05:42 -0800

    SVT4-26 complete - API to get a single inventory

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -1,5 +1,3 @@
-const { json } = require("express");
-
 const knex = require("knex")(require("../knexfile"));
 
 const index = async (_req, res) => {
@@ -11,6 +9,26 @@ const index = async (_req, res) => {
   }
 };
 
+const findOne = async (req, res) => {
+  try {
+    const data = await knex("inventories").where({
+      id: req.params.id,
+    }); //getting users from database with id
+    if (data.length === 0) {
+      return res.status(404).json({
+        message: `Error retrieving inventory ${req.params.id}`,
+      });
+    }
+    const inventoryData = data[0];
+    res.status(200).json(inventoryData);
+  } catch (error) {
+    res.status(404).json({
+      message: `Unable to retrieve inventory data for inventory with ID ${req.params.id}`,
+    });
+  }
+};
+
 module.exports = {
   index,
+  findOne,
 };
--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -1,8 +1,7 @@
-const express = require("express");
-const router = express.Router();
-
+const router = require("express").Router();
 const inventoryController = require("../controllers/inventory-controller");
 
 router.route("/").get(inventoryController.index);
+router.route("/:id").get(inventoryController.findOne);
 
 module.exports = router;
