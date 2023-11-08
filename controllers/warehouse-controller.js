26231a078716f53310291b61df543750be8c611d
Stephen Im <imssbb@gmail.com>
2023-11-07 22:26:01 -0800

    add post/create new warehouse, TICKETSVT4-16

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -1,4 +1,5 @@
 const knex = require('knex')(require('../knexfile'));
+const validator = require('validator');
 
 // Get All Warehouse Information
 const index = async (_req, res) => {
@@ -29,4 +30,57 @@ const findOne = async (req, res) => {
   }
 };
 
-module.exports = { index, findOne };
+// Post/Create New Warehouse
+
+function isValidEmail(email) {
+  //   return va.includes('@'); // For a basic example, let's check if the email contains an @ symbol
+  return validator.isEmail(email);
+}
+
+function isValidPhoneNumber(phoneNumber) {
+  // Use the isMobilePhone function with specific locale and strictMode settings
+  return validator.isMobilePhone(phoneNumber, 'en-US', { strictMode: false });
+}
+
+const add = async (req, res) => {
+  if (
+    !req.body.warehouse_name ||
+    !req.body.address ||
+    !req.body.city ||
+    !req.body.country ||
+    !req.body.contact_name ||
+    !req.body.contact_email
+  ) {
+    return res.status(400).json({
+      message: 'Unsuccessful. Please provide missing information',
+    });
+  }
+
+  if (!isValidEmail(req.body.contact_email)) {
+    return res.status(400).json({ message: 'Invalid email address' });
+  }
+
+  if (!isValidPhoneNumber(req.body.contact_phone)) {
+    return res.status(400).json({ message: 'Invalid phone number' });
+  }
+  try {
+    const result = await knex('warehouses').insert(req.body);
+    // console.log('req.body:', req.body);
+
+    const newWarehouseId = result[0];
+    const createWarehouse = await knex('warehouses').where({
+      id: newWarehouseId,
+    });
+    res.status(201).json(createWarehouse);
+  } catch (err) {
+    res.status(500).json({
+      message: `Unable to create new warehouse: ${err}`,
+    });
+  }
+};
+
+//Put/Edit Warehouse
+
+// Delete Warehouse
+
+module.exports = { index, findOne, add };
--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const useController = require('../controllers/warehouse-controller');
 
-router.route('/').get(useController.index);
+router.route('/').get(useController.index).post(useController.add);
 router.route('/:id').get(useController.findOne);
 
 module.exports = router;
